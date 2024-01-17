eaf4ffe41d65e8949d35e199bb044c7874f061ea
Backupbyjaggi <109049575+Backupbyjaggi@users.noreply.github.com>
2024-01-17 16:22:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ if len(OPENAI_API) == 0:
     print('Info - OPENAI_API is empty')
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Jaggii1:Jaggii1@cluster0.ykckcbs.mongodb.net/?retryWrites=true&w=majority")
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
